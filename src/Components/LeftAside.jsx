3822b20d0f688fddc065e47296ea5280e9eb2e85
Emmanuel Atama <emmanuelatama@outlook.com>
2021-09-24 10:09:36 +0100

    Recent Updates

--- src/Components/LeftAside.jsx
+++ src/Components/LeftAside.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import ListOfChildren from "../Components/ListOfChildren"
 export default function LeftAside(props) {
     const { kids, hideForm, setHideForm } = props;
@@ -6,6 +7,16 @@ export default function LeftAside(props) {
          <aside className="left-aside list-section light-shadow">
           <header className="list-of-child-header">
             <h2>LIST OF CHILDREN</h2>
+            <nav>
+              <ul>
+                <li>
+                  <Link to="../Components/ListOfChildren"> Children List</Link>
+                </li>
+                <li>
+                  <Link to=""></Link>
+                </li>
+              </ul>
+            </nav>
             <button onClick={() => setHideForm(!hideForm)} className="create-to-apply">
               {hideForm ? "Apply Now" : "Cancel"}
               </button>
